4b7e203d6b90f1b568624f88d96bf24843583f3f
soukev <soukev@soukev.xyz>
2023-06-08 18:10:17 +0200

    Update package with link to github

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 DESCRIPTION = "Package providing functional error handling."
 LONG_DESCRIPTION = "Package provinding functional error handling in 'monadic'-like way. Allows you to create wrapped value from success value or an error, apply functions to wrapped value and unwrap."
 
@@ -9,6 +9,7 @@ setup(
     version=VERSION,
     author="Vojtech Soukenka",
     author_email="<vojtech.soukenka@gmail.com>",
+    url="https://github.com/soukev/pyerr",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages= find_packages(),
